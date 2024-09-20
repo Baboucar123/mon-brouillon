283bd273d41722300352ec388ed8090ea069c2e2
Baboucar123 <coly.baboucar.ab@gmail.com>
2024-09-20 15:24:54 +0200

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
+int choix;
+
 Client::Client(string n, string p, int code1, int code2)
-    : nom(n), prenom(p), compte1(1, 1000.0, code1), compte2(2, 500.0, code2) {}
+    : nom(n), prenom(p), compte1(1, 1000.0, code1), compte2(2, 500.0, code2) {
+        
+    }
 
 bool Client::demander_operation() {
     char reponse;
@@ -20,26 +24,28 @@ bool Client::demander_operation() {
 }
 
 void Client::menu_client() {
+
+    cout << "Bienvenue " << prenom << " " << nom << " !\n";
     // Demander d'abord si l'utilisateur veut faire une opération
     if (!demander_operation()) {
         return;  // Si l'utilisateur dit non, on arrête ici.
     }
 
-    int choix;
-    cout << "Bienvenue " << prenom << " " << nom << " !\n";
+  
+    
     cout << "Que souhaitez-vous faire ?\n";
     cout << "1. Gerer le compte 1\n";
     cout << "2. Gerer le compte 2\n";
-    cout << "3. Effectuer un virement entre vos comptes\n";
+
     cin >> choix;
 
     if (choix == 1) {
         gerer_compte(compte1);
     } else if (choix == 2) {
         gerer_compte(compte2);
-    } else if (choix == 3) {
-        virement_entre_comptes();
-    } else {
+    } 
+   
+  else {
         cout << "Choix non valide.\n";
     }
 
@@ -60,6 +66,7 @@ void Client::gerer_compte(Compte &compte) {
             cout << "1. Consulter le solde\n";
             cout << "2. Crediter le compte\n";
             cout << "3. Debiter le compte\n";
+            cout << "4. effectuer un virement vers un autre compte\n";
             cout << "0. Quitter\n";
             cin >> option;
 
@@ -77,6 +84,9 @@ void Client::gerer_compte(Compte &compte) {
                     cin >> montant;
                     compte.debiter(montant);
                     break;
+                    case 4 :
+                    virement_entre_comptes() ;
+                    break; 
                 case 0:
                     cout << "Deconnexion du compte.\n";
                     break;
@@ -91,29 +101,18 @@ void Client::gerer_compte(Compte &compte) {
 }
 
 void Client::virement_entre_comptes() {
-    int code, option;
+    
     double montant;
-
-    cout << "Veuillez saisir le code secret : ";
-    cin >> code;
-
-    // Vérifier si le code est correct pour l'un des comptes
-    if (compte1.verifier_code(code) || compte2.verifier_code(code)) {
         cout << "Montant du virement : ";
         cin >> montant;
-        cout << "Effectuer le virement :\n";
-        cout << "1. Du compte 1 vers le compte 2\n";
-        cout << "2. Du compte 2 vers le compte 1\n";
-        cin >> option;
+      
 
-        if (option == 1) {
+        if (choix== 1) {
             compte1.virement(compte2, montant);  // Virement de compte1 à compte2
-        } else if (option == 2) {
+        } else if (choix== 2) {
             compte2.virement(compte1, montant);  // Virement de compte2 à compte1
         } else {
             cout << "Option non valide.\n";
         }
-    } else {
-        cout << "Code secret incorrect.\n";
-    }
+    
 }
